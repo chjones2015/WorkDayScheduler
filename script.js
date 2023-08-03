9d7f03f4a4df625b94ee5dbfb6ab5af08a062742
Christian Jones <chjones2015@gmail.com>
2023-08-02 19:08:43 -0500

    fixed some steps

--- script.js
+++ script.js
@@ -3,15 +3,43 @@
 // in the html.
 $(function () {
 
-  const currentDate = moment().format("dddd, MMMM Do YYYY");
-  const dateDisplay = document.getElementById("currentDay");
-  dateDisplay.textContent = currentDate;
-  
-  const currentHour = moment().hour();
-  
-  const timeBlocks = Array.from({length: 9}, (_, i) => i + 9);
-  
-  const planner = document.getElementById("planner");
+// TODO 1: Get the current date and display it at the top of the calendar
+const currentDate = moment().format("dddd, MMMM Do YYYY");
+const dateDisplay = document.getElementById("currentDay");
+dateDisplay.textContent = currentDate;
+
+// TODO 2: Create time blocks for standard business hours of 9am to 5pm
+const timeBlocks = Array.from({length: 9}, (_, i) => i + 9);
+const planner = document.getElementById("planner");
+
+// TODO 3: Color-code each time block based on whether it is in the past, present, or future
+const currentHour = moment().hour();
+timeBlocks.forEach(hour => {
+  const timeBlock = document.createElement("div");
+  timeBlock.classList.add("time-block");
+  if (hour < currentHour) {
+    timeBlock.classList.add("past");
+  } else if (hour === currentHour) {
+    timeBlock.classList.add("present");
+  } else {
+    timeBlock.classList.add("future");
+  }
+  const hourDisplay = document.createElement("div");
+  hourDisplay.classList.add("hour");
+  hourDisplay.textContent = moment(hour, "H").format("hA");
+  const description = document.createElement("textarea");
+  description.classList.add("description");
+  const saveButton = document.createElement("button");
+  saveButton.classList.add("saveBtn");
+  saveButton.textContent = "Save";
+  timeBlock.appendChild(hourDisplay);
+  timeBlock.appendChild(description);
+  timeBlock.appendChild(saveButton);
+  planner.appendChild(timeBlock);
+});
+
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
